d2f6378eaf6900940d93d346e84819e4bac0b382
Tonn-nnay <tymartinmarin@gmail.com>
2025-10-16 11:35:06 +0200

    View structure at app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,39 @@
 <script setup>
+import { ref } from 'vue';
 import GameEngine from './components/GameEngine.vue';
 import Teclat from './components/Teclat.vue';  
+
+const vistaActual = ref('salaEspera'); // 'salaEspera', 'lobby', 'joc'
+
+const nomJugador = ref('');
+const jugadors = ref([]);
+let socket = null;
 </script>
 
 <template>
-  <GameEngine />
-  <Teclat />
+  <main>
+    <!-- VISTA 1: SALA D'ESPERA -->
+    <div v-if="vistaActual === 'salaEspera'" class="vista-container">
+      <h1>Type Racer Royale</h1>
+      <input type="text" v-model="nomJugador" placeholder="Introdueix el teu nom" />
+      <button @click="connectarAlServidor">Entra al Lobby</button>
+    </div>
+
+    <!-- VISTA 2: LOBBY -->
+    <div v-else-if="vistaActual === 'lobby'" class="vista-container">
+      <h2>Jugadors Connectats</h2>
+      <ul>
+        <li v-for="jugador in jugadors" :key="jugador.id">{{ jugador.name }}</li>
+      </ul>
+      <button @click="vistaActual = 'joc'">Comença a Jugar!</button>
+    </div>
+
+    <!-- VISTA 3: JOC -->
+    <div v-else-if="vistaActual === 'joc'" class="vista-container">
+      <GameEngine />
+      <Teclat />
+    </div>
+  </main>
 </template>
 
 <style scoped>
